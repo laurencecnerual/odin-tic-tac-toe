256c3dbfb243b466231be9d58686cd8b2e543311
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2024-07-23 12:07:07 +0900

    Adds GameMaster, begins implementing turn system

--- script.js
+++ script.js
@@ -1,51 +1,69 @@
-const gameBoard = (function () {
-    const tttSize = 3;
-    let board;
+const gameMaster = (function () {
+    const gameBoard = (function () {
+        const tttSize = 3;
+        let board;
 
-    const cleanBoard = () => {
-        board = [];
-        for (let x = 0; x < tttSize; x++) {
-            board[x] = [];
-            for (let y = 0; y < tttSize; y++) {
-                board[x][y] = " ";
+        const cleanBoard = () => {
+            board = [];
+            for (let x = 0; x < tttSize; x++) {
+                board[x] = [];
+                for (let y = 0; y < tttSize; y++) {
+                    board[x][y] = " ";
+                }
             }
         }
-    }
 
-    cleanBoard();
+        cleanBoard();
 
-    const fillCell = (x, y, value) => {
-        if (board[x][y] == " ") {
-            board[x][y] = value;
+        const fillCell = (x, y, value) => {
+            if (board[x][y] == " ") {
+                board[x][y] = value;
+                return true; //succeeded in filling cell because it was empty
+            } else {
+                return false; //failed to fill cell because it was already full
+            }
         }
+
+        const getBoard = () => {
+            return board;
+        }
+
+        const logBoard = () => {
+            console.log(board[0] + "\n" + board[1] + "\n" + board[2]);
+        }
+
+        return {getBoard, cleanBoard, fillCell, logBoard};
+    })();
+
+    function createPlayer(name, team) {
+        const getName = () => name;
+        const getTeam = () => team;
+        let score = 0;
+        const getScore = () => score;
+        const incrementScore = () => score++;
+
+        return {getName, getTeam, getScore, incrementScore};
     }
 
-    const getBoard = () => {
-        return board;
+    const players = [];
+    players.push(createPlayer("Player 1", "X"));
+    players.push(createPlayer("Player 2", "O"));
+    
+    function determineActivePlayerIndex() {
+        let turnNumber = 0;
+        return () => {
+            return turnNumber++ % players.length; //Expected to always return 0 or 1 since tic tac toe is a 2 player game
+        };
     }
+    
+    const activePlayerIndex = determineActivePlayerIndex();
+    console.log(activePlayerIndex());
+    console.log(activePlayerIndex());
+    console.log(activePlayerIndex());
+    console.log(activePlayerIndex());
 
-    const logBoard = () => {
-        console.log(board[0] + "\n" + board[1] + "\n" + board[2]);
-    }
-
-    return {getBoard, cleanBoard, fillCell, logBoard};
+    gameBoard.fillCell(2,2,players[0].getTeam());
+    gameBoard.fillCell(1,1,players[1].getTeam());
+    gameBoard.fillCell(0,0,players[0].getTeam());
+    gameBoard.logBoard();
 })();
-
-function createPlayer(name, team) {
-    const getName = () => name;
-    const getTeam = () => team;
-    let score = 0;
-    const getScore = () => score;
-    const incrementScore = () => score++;
-
-    return {getName, getTeam, getScore, incrementScore};
-}
-
-const players = [];
-players.push(createPlayer("Player 1", "X"));
-players.push(createPlayer("Player 2", "O"));
-
-gameBoard.fillCell(2,2,players[0].getTeam());
-gameBoard.fillCell(1,1,players[1].getTeam());
-gameBoard.fillCell(0,0,players[0].getTeam());
-gameBoard.logBoard();
